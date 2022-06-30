c7a6c14fe0777e42f53a697bffe4c3d663c31ad3
joemoseti <joelmoseti0@gmail.com>
2022-06-30 16:46:14 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - Print a string
+ * Printf is used to print the output
+ * Return: 0
+ */
+
+
+
+int main(void)
+
+{
+
+		printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}
