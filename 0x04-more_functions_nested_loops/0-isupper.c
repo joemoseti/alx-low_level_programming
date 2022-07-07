681fae02d4c37f65b2d94ed32ee51a82c47446a2
joemoseti <joelmoseti0@gmail.com>
2022-07-07 14:29:41 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,29 +1,15 @@
 #include "main.h"
-
 /**
+ * _isupper - function
  *
- *  * _isupper - function
+ * @c: parameter
  *
- *   *
- *
- *    * @c: parameter
- *
- *     *
- *
- *      * Return: return value
- *
- *       */
-
+ * Return: return value
+ */
 int _isupper(int c)
-
 {
-
-		if (c >= 65 && c <= 90)
-
-					return (1);
-
-			else
-
-						return (0);
-
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
 }
