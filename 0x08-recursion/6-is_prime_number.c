be09dc2bc3be0bc6122e65bb847a294711d7241d
joemoseti <joelmoseti0@gmail.com>
2022-07-20 12:21:21 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,27 +1,27 @@
-#include "return 1 or 0main.h"
+#include "main.h"
 /**
- *is_prime_number - returns 1 if the input integer is a prime number
- *@i: integer
- *@n: integer
+ *evaluate_num - recursion loop
+ *@num: num
+ *@iterator: number to iterate
  *Return: return 1 or 0
  */
 
-int is_prime_number(int i, int n)
+int evaluate_num(int num, int iterator)
 {
 
-	if (n == i - 1)
-	{
-	return (1);
-	}
-
-	else if (i % iterator == 0)
-	{
-	return (0);
-	}
-
-if (i % n != 0)
+if (iterator == num - 1)
 {
-return (evaluate_i(i, n + 1));
+return (1);
+}
+
+else if (num % iterator == 0)
+{
+return (0);
+}
+
+if (num % iterator != 0)
+{
+return (evaluate_num(num, iterator + 1));
 }
 
 return (0);
@@ -29,28 +29,28 @@ return (0);
 }
 
 /**
- *is_prime_number -  returns 1 if the input integer is a prime number
- *@n: integer
- *Return: return 1 or 0
+ *is_prime_number - evaluate prime or not
+ *@num: number
+ *Return: return 1 prime - return 0 otherwise
  */
 
-int is_prime_number(int i)
+int is_prime_number(int num)
 {
 
-	int n;
+int iterator;
 
-	n = 2;
+iterator = 2;
 
-	/* only greater than 2*/
-	if (i < 2)
-	{
-		return (0);
-	}
+/* only greater than 2*/
+if (num < 2)
+{
+return (0);
+}
 
-	if (i == 2)
-	{
-		return (i);
-	}
+if (num == 2)
+{
+return (1);
+}
 
-	return (evaluate_i(i, n));
+return (evaluate_num(num, iterator));
 }
