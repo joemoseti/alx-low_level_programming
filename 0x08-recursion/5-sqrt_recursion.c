ff3c2aa5259273e78c5af035b770fc0d48a41d4d
joemoseti <joelmoseti0@gmail.com>
2022-07-20 06:17:20 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,22 @@
+nclude "main.h"
+/**
+ *_sqrt_recursion - returns the natural sqaure root of a number
+ *@n: integer 
+ *Return: sqrt recursion
+*/
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0 || n == !) /*Evaluate function*/
+		return (n);
+
+	else if (i * i < n)
+		return (_evaluated(i + 1, n));
+
+	else if (i * i == n) /*Condition base*/
+		return (i);
+
+	return (-1);
+
+	return (-1);
+}
