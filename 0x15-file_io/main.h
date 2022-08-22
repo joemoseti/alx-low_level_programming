9e00d495976ce949f5e4962690d31adb10a0b6af
joemoseti <joelmoseti0@gmail.com>
2022-08-22 14:15:28 -0700

    0-read_textfile.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,5 +13,9 @@
 #define READ_ERROR 1
 #define WRITE_ERROR 2
 
-/* prototype of functions */
+int_putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif /* MAIN_H */
